e2482a05c95b96868b3fc4b3121476838c23c815
dmsry27 <111333708+dmsry27@users.noreply.github.com>
2022-11-07 09:10:46 +0900

    수정중

--- C_Kang/Assets/Scripts/Player/Player.cs
+++ C_Kang/Assets/Scripts/Player/Player.cs
@@ -2,30 +2,103 @@ using System;
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;     
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public float rotateSpeed = 30.0f;
 
-    bool isMove = false;
+    bool isPlay = false;
 
     PlayerInputSystem input;
     Animator ani;
     Rigidbody rigid;
     Warrior warrior;
-    WaitForSeconds q_Cooltime;
-    
+
     Vector3 clickPositon;
+    PlayerState state;
+
+    Action updateState;
+
+    enum PlayerState
+    {
+        Idle = 0,
+        Run,
+        Dive,
+        Attack,
+        Skill,
+        Die
+    }
+
+    PlayerState State
+    {
+        get => state;
+        set
+        {
+            switch (state)
+            {
+                case PlayerState.Idle:
+                    break;
+                case PlayerState.Run:
+                    ani.applyRootMotion = true;
+                    ani.SetBool("IsMove", false);
+                    break;
+                case PlayerState.Dive:
+                    break;
+                case PlayerState.Attack:
+                    break;
+                case PlayerState.Skill:
+                    break;
+                case PlayerState.Die:
+                    break;
+                default:
+                    break;
+            }
+            state = value;
+            switch (state)
+            {
+                case PlayerState.Idle:
+                    updateState = Update_Idle;
+                    break;
+                case PlayerState.Run:
+                    ani.applyRootMotion = false;
+                    ani.SetBool("IsMove", true);
+                    updateState = Update_Run;
+                    break;
+                case PlayerState.Dive:
+                    ani.SetTrigger("Dive");
+                    isPlay = true;
+                    updateState = Update_Dive;
+                    break;
+                case PlayerState.Attack:
+                    ani.SetTrigger("Attack");
+                    isPlay = true;
+                    updateState = Update_Attack;
+                    break;
+                case PlayerState.Skill:
+                    updateState = Update_Skill;
+                    break;
+                case PlayerState.Die:
+                    updateState = Update_Die;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
     private void Awake()
     {
         input = new PlayerInputSystem();
-        q_Cooltime = new WaitForSeconds(2.0f);
         ani = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody>();
         warrior = GetComponent<Warrior>();
+    }
+
+    private void Start()
+    {
+        State = PlayerState.Idle;
     }
 
     private void OnEnable()
@@ -56,10 +129,7 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (isMove)
-        {
-            Move();
-        }
+        updateState();
     }
 
     private void OnMouseClick(InputAction.CallbackContext _)
@@ -70,57 +140,61 @@ public class Player : MonoBehaviour
         // GetMask로는 찾아지고 NameToLayer로는 못찾는다??
         {
             clickPositon = hit.point;
-            isMove = true;
+            State = PlayerState.Run;
         }
     }
 
-    void Move()
-    {
-        Vector3 dir = clickPositon - transform.position;
-        if (dir.sqrMagnitude > 0.1f)
-        {
-            rigid.MovePosition(rigid.position + moveSpeed * Time.fixedDeltaTime * dir.normalized);
-            ani.SetBool("IsMove", isMove);
-        }
-        else
-        {
-            isMove = false;
-            ani.SetBool("IsMove", isMove);
-        }
-        rigid.rotation = Quaternion.Slerp(rigid.rotation, Quaternion.LookRotation(dir), rotateSpeed * Time.fixedDeltaTime);
-    }
-
-    private void OnAttack(InputAction.CallbackContext obj)
-    {
+    private void OnAttack(InputAction.CallbackContext _)
+    { 
+        State = PlayerState.Attack;
     }
 
     private void OnDive(InputAction.CallbackContext _)
     {
-        ani.SetTrigger("Dive");
-        isMove = false;
-        ani.SetBool("IsMove", isMove);
-        input.Player.Disable();
-    }
-    
-    public void OffDive()
-    {
-        input.Player.Enable();
+         State = PlayerState.Dive;
     }
 
-    IEnumerator Q_Cooldown()
+    void Update_Idle()
     {
-        input.Player.Q_Skill.performed -= On_Q;
-        yield return q_Cooltime;
-        input.Player.Q_Skill.performed += On_Q;
+
+    }
+
+    void Update_Run()
+    {
+        Vector3 dir = clickPositon - transform.position;
+
+        if (dir.sqrMagnitude > 0.1f)
+        {
+            rigid.MovePosition(rigid.position + moveSpeed * Time.fixedDeltaTime * dir.normalized);
+        }
+        else
+        {
+            State = PlayerState.Idle;
+        }
+        rigid.rotation = Quaternion.Slerp(rigid.rotation, Quaternion.LookRotation(dir), rotateSpeed * Time.fixedDeltaTime);
+    }
+
+    void Update_Dive()
+    {
+        
+    }
+
+    void Update_Attack()
+    {
+
+    }
+    void Update_Skill()
+    {
+
+    }
+    void Update_Die()
+    {
+
     }
 
     private void On_Q(InputAction.CallbackContext _)
     {
-        Vector3 dir = transform.rotation * Vector3.forward;
-        Quaternion.LookRotation(dir);
-        clickPositon = transform.position;
         warrior.Q_Skill();
-        StartCoroutine(Q_Cooldown());
     }
 
     private void On_W(InputAction.CallbackContext _)
